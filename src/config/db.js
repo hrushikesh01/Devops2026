c75d9374422edaf5d75f03a4b754483aeca75c84
hrushikesh01 <166029552+hrushikesh01@users.noreply.github.com>
2026-01-28 16:19:56 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-  logging: false, // set true if you want SQL logs
-});
-
-module.exports = sequelize;
